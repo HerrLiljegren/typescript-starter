36a344dee0aacd125361f68d4565a40b453d4bfa
Jesper Liljegren <jesper.liljegren@gmail.com>
2016-12-19 16:56:52 +0100

    Using gulp-develop-server to launch a watched express-server

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # typescript-starter
 Simple typescript-nodejs-express starter template
+http://mherman.org/blog/2016/11/05/developing-a-restful-api-with-node-and-typescript/
--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,37 @@
 var gulp = require('gulp');
-var server = require('gulp-develop-server');
+var nodemon = require('gulp-nodemon');
 var ts = require('gulp-typescript');
 var tsProject = ts.createProject('tsconfig.json');
 
-// Copy files
-gulp.task('copy-files', () => {
-	return gulp.src(['./src/data.json']).pipe(gulp.dest('dist'));
+// The default command will build and start the express-server
+gulp.task('default', ['server:start']);
+
+// Build and start the express-server, rebuild on changes
+gulp.task('server:start', ['build'], function () {
+	nodemon({
+		env: { 'NODE_ENV': 'development' },
+		ext: 'ts',
+		script: 'dist/index.js',
+		tasks: ['build'],
+	});
 });
 
-gulp.task('copy-public', () => {
-	return gulp.src(['./src/public/**']).pipe(gulp.dest('dist/public'));
-});
+// Complete build-step
+gulp.task('build', ['compile', 'copy-files', 'copy-public']);
 
-// Transpile the Typescript-code into Javascript
-gulp.task('scripts', () => {
+// Compile the Typescript-code into Javascript
+gulp.task('compile', () => {
 	const tsResult = tsProject.src()
 		.pipe(tsProject());
 	return tsResult.js.pipe(gulp.dest('dist'));
 });
 
-// Setup watch on all .ts, .js and public-files
-gulp.task('watch', ['scripts'], () => {
-	gulp.watch(['src/**/*.ts'], ['scripts']);
-	gulp.watch(['src/public/**'], ['copy-public']);
-	gulp.watch(['./dist/index.js'], [server.restart]);
+// Copy public-folder
+gulp.task('copy-public', () => {
+	return gulp.src(['./src/public/**']).pipe(gulp.dest('dist/public'));
 });
 
-gulp.task('build', ['scripts', 'copy-files', 'copy-public']);
-
-// Start the express-server
-gulp.task('server:start', () => {
-	server.listen({ path: './dist/index.js' });
+// Copy files
+gulp.task('copy-files', () => {
+	return gulp.src(['./src/data.json']).pipe(gulp.dest('dist'));
 });
-
-// The default command will build and start the express-server
-gulp.task('default', ['server:start']);
